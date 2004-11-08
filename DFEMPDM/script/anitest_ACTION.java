5410e8531b38cd200b3b08e0eec6bbb7e53a4b96
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2004-11-08 02:25:09 +0000

    Initial commit
    git-svn-id: svn://localhost/dfempdm/trunk@13 5488ef97-eb99-974b-8148-b326c9ec6504

---
+++ DFEMPDM/script/anitest_ACTION.java
@@ -0,0 +1,18 @@
+
+//Begin of BeanShell -- anitest_Action
+import dyna.framework.service.DOS;
+import dyna.framework.service.DSS;
+import dyna.framework.service.dos.DOSChangeable;
+import dyna.framework.client.DynaMOAD;
+
+import dyna.framework.client.dfempdm.anitest;
+import dyna.framework.client.dfempdm.HowTo;
+
+
+{
+//	DOSChangeable dosObject = DynaMOAD.dos.get("operation$sf@8605de1a");
+//	print(dosObject);
+//	anitest.perform (inputObject, null);
+//	anitest.perform (inputObject, returnObject);
+    HowTo.useRoutingEditor(inputObject);
+}

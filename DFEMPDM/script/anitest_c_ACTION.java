52c924b791980ffca984e2b688b5db6a03f227a5
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2004-11-11 03:21:18 +0000

    Initial commit
    git-svn-id: svn://localhost/dfempdm/trunk@22 5488ef97-eb99-974b-8148-b326c9ec6504

---
+++ DFEMPDM/script/anitest_c_ACTION.java
@@ -0,0 +1,14 @@
+
+//Begin of BeanShell -- anitest_Action
+import dyna.framework.service.DOS;
+import dyna.framework.service.DSS;
+import dyna.framework.service.dos.DOSChangeable;
+import dyna.framework.client.DynaMOAD;
+
+import dyna.framework.client.dfempdm.anitest;
+import dyna.framework.client.dfempdm.HowTo;
+
+
+{
+    HowTo.useMaterialCalculator(inputObject);
+}
